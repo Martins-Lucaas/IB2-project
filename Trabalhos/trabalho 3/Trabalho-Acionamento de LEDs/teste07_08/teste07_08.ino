7502e01878c2859cb5686fa6f2645d8f23913e88
renagod <94547891+renagod@users.noreply.github.com>
2024-08-26 19:35:29 -0300

    portas
    portas output

--- Trabalhos/trabalho 3/Trabalho-Acionamento de LEDs/teste07_08/teste07_08.ino
+++ Trabalhos/trabalho 3/Trabalho-Acionamento de LEDs/teste07_08/teste07_08.ino
@@ -1,10 +1,10 @@
 #include <Arduino.h>
-#define LED1_PIN 32
-#define LED2_PIN 35
+#define LED1_PIN 19
+#define LED2_PIN 2
 volatile uint8_t ledState = 0;
 volatile int sensorValue = 1;
 void setup() {
-  Serial.begin(921600);
+  Serial.begin(115200);
   pinMode(LED1_PIN, OUTPUT);
   pinMode(LED2_PIN, OUTPUT);
 
@@ -18,8 +18,9 @@ void toggleLEDs(void *parameter) {
   while (true) {
     ledState = (ledState + 1) % 3;
     digitalWrite(LED1_PIN, (ledState == 0) ? HIGH : LOW);
-    digitalWrite(LED2_PIN, (ledState == 2) ? LOW : HIGH);
-    vTaskDelay(20 / portTICK_PERIOD_MS);
+    //Serial.print(digitalRead(34));
+    digitalWrite(LED2_PIN, (ledState == 2) ? HIGH : LOW);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
