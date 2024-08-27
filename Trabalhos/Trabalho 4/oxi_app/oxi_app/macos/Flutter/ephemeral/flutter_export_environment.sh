4c4b804bc6b2fa3ca488288c8602ba79b56d532f
renagod <94547891+renagod@users.noreply.github.com>
2024-08-27 13:42:57 -0300

    sei la
    se fode ai martins

---
+++ Trabalhos/Trabalho 4/oxi_app/oxi_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\renas\OneDrive\Documentos\teste\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\renas\OneDrive\Documentos\IB2\IB2-project\Trabalhos\Trabalho 4\oxi_app\oxi_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
