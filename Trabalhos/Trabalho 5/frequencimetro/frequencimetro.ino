3042d3f7e3a0198b1bf56f1072e2eb2fa95e8d55
renagod <94547891+renagod@users.noreply.github.com>
2024-10-02 11:09:23 -0300

    código de aula
    testes realizados em aula

---
+++ Trabalhos/Trabalho 5/frequencimetro/frequencimetro.ino
@@ -0,0 +1,58 @@
+#include "driver/pcnt.h"
+#include "esp_timer.h"
+
+#define SIGNAL_PIN 15  // Pino de entrada para o sinal da balança
+#define PCNT_UNIT PCNT_UNIT_0
+
+esp_timer_handle_t timer_handle;
+int16_t pulse_count = 0;
+
+void timer_callback(void* arg) {
+  pcnt_get_counter_value(PCNT_UNIT, &pulse_count);
+  
+
+
+  pcnt_counter_clear(PCNT_UNIT);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+
+  pcnt_config_t pcnt_config;
+  pcnt_config.pulse_gpio_num = SIGNAL_PIN;
+  pcnt_config.ctrl_gpio_num = PCNT_PIN_NOT_USED;
+  pcnt_config.channel = PCNT_CHANNEL_0;
+  pcnt_config.unit = PCNT_UNIT;
+  pcnt_config.pos_mode = PCNT_COUNT_INC;
+  pcnt_config.neg_mode = PCNT_COUNT_DIS;
+  pcnt_config.lctrl_mode = PCNT_MODE_KEEP;
+  pcnt_config.hctrl_mode = PCNT_MODE_KEEP;
+  pcnt_config.counter_h_lim = 4294967290; 
+  pcnt_config.counter_l_lim = -4294967290;
+
+  pcnt_unit_config(&pcnt_config);
+
+  pcnt_counter_pause(PCNT_UNIT);
+  pcnt_counter_clear(PCNT_UNIT);
+  pcnt_counter_resume(PCNT_UNIT);
+
+
+  const esp_timer_create_args_t timer_args = {
+    .callback = &timer_callback,
+    .name = "frequency_timer"
+  };
+
+  esp_timer_create(&timer_args, &timer_handle);
+  esp_timer_start_periodic(timer_handle, 1000000);
+}
+
+void loop() {
+    // Calcula a frequência
+  unsigned long int frequency = pulse_count ;
+
+  Serial.print("Frequência: ");
+  Serial.print(frequency);
+  Serial.println(" Hz");
+  
+}
