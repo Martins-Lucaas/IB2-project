4cf0eb05f2bc5d2b0d25cb7e364cd48897e04c90
Lucas_Martins <lucaspmartins14@gmail.com>
2024-10-21 12:11:13 -0300

    organização

--- Trabalhos/Trabalho 5/frequencimetro/frequencimetro.ino
+++ Trabalhos/Trabalho 5/frequencimetro/frequencimetro.ino
@@ -3,6 +3,7 @@
 
 #define SIGNAL_PIN 15  // Pino de entrada para o sinal da balança
 #define PCNT_UNIT PCNT_UNIT_0
+#define PIN 13
 
 esp_timer_handle_t timer_handle;
 int16_t pulse_count = 0;
@@ -17,7 +18,7 @@ void setup() {
 
   pcnt_config_t pcnt_config;
   pcnt_config.pulse_gpio_num = SIGNAL_PIN;
-  pcnt_config.ctrl_gpio_num = PCNT_PIN_NOT_USED;
+  pcnt_config.ctrl_gpio_num = PIN;
   pcnt_config.channel = PCNT_CHANNEL_1;
   pcnt_config.unit = PCNT_UNIT;
   
@@ -26,7 +27,7 @@ void setup() {
   pcnt_config.lctrl_mode = PCNT_MODE_KEEP;
   pcnt_config.hctrl_mode = PCNT_MODE_KEEP;
   pcnt_config.counter_h_lim = 100000;
-  pcnt_config.counter_l_lim = 0.1;
+  pcnt_config.counter_l_lim = 0;
   
   pcnt_unit_config(&pcnt_config);
 
@@ -39,11 +40,11 @@ void setup() {
     .name = "frequency_timer"
   };
   esp_timer_create(&timer_args, &timer_handle);
-  esp_timer_start_periodic(timer_handle, 100000);
+  esp_timer_start_periodic(timer_handle, 1000);
 }
 void loop() {
     // Calcula a frequência
-  unsigned long int frequency = pulse_count*100 ;
+  unsigned long long int frequency = pulse_count*1000 ;
   Serial.print("Frequência: ");
   Serial.print(frequency);
   Serial.println(" Hz");
